bf36198cf3313b35c61d9561a3c274986c622713
上田あすか <asukas.ueda@gmail.com>
2020-06-08 18:21:03 +0900

    ハンバーガーメニュー実装途中。

--- js/drawer.js
+++ js/drawer.js
@@ -10,3 +10,17 @@ $(function(){
         $(setImg + ' :first-child').animate({opacity:'0'},fadeSpeed).next('img').animate({opacity:'1'},fadeSpeed).end().appendTo(setImg);
     },switchDelay);
 });
+
+function toggleNav() {
+    var body = document.body;
+    var hamburger = document.getElementById('target_inner');
+    var blackBg = document.getElementById('target_open');
+
+    hamburger.addEventListener('click', function () {
+        body.classList.toggle('menu_open');
+    });
+    blackBg.addEventListener('click', function () {
+        body.classList.remove('menu_open');
+    });
+}
+toggleNav();
