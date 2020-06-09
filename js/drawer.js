da3e9a9b2bef07ac40d3fef8d7496ac0b8a5a0d9
上田あすか <asukas.ueda@gmail.com>
2020-06-09 18:24:13 +0900

    レスポンシブ画面のナビメニュー の調整・基本情報欄とコンタクト欄の調整途中。

--- js/drawer.js
+++ js/drawer.js
@@ -11,16 +11,14 @@ $(function(){
     },switchDelay);
 });
 
-function toggleNav() {
-    var body = document.body;
-    var hamburger = document.getElementById('target_inner');
-    var blackBg = document.getElementById('target_open');
+function hamburger() {
+    　　document.getElementById('line1').classList.toggle('linea');
+    　　document.getElementById('line2').classList.toggle('lineb');
+    　　document.getElementById('line3').classList.toggle('linec');
+    　　document.getElementById('target').classList.toggle('slidex');
+    　　document.getElementById('nav_f').classList.toggle('fadein');
+    }
 
-    hamburger.addEventListener('click', function () {
-        body.classList.toggle('menu_open');
+    document.getElementById('target').addEventListener('click', function () {
+    　　hamburger();
     });
-    blackBg.addEventListener('click', function () {
-        body.classList.remove('menu_open');
-    });
-}
-toggleNav();
